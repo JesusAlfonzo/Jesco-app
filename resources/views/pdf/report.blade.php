a364a986c4b3b9c9ee8e4414eaffff10775caf62
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2025-05-23 11:27:36 -0400

    PDF's Templates View Created

---
+++ resources/views/pdf/report.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Report Page</title>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+
+<!-- 
+Nota: Esta vista es la plantilla general para el PDF.
+      Solo se va a trabajar cuando el proyecto este mas avanzado.
+      De momento no tenemos ni usuarios de pruebas ni formas de testear el PDF.
+      Tomar en cuenta la guia del video de "Code With Luis"
+-->
+
